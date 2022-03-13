2c684da48dc7b08d823e1999a0c3b993000f606f
UpDJ <deionshallenberger@UCSF-MacBook-Pro.local>
2022-03-13 14:15:58 -0700

    database init button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 
 import Register from './pages/registration';
 import { Login } from './pages/login';
+import Initialize from './components/button';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Initialize/>
     </div>
   );
 }
